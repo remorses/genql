e83e5fea50b38ff59c58c27408ee79569e8750e9
remorses <beats.by.morse@gmail.com>
2020-05-23 13:12:27 +0200

    website/constants: show node env

--- website/constants.ts
+++ website/constants.ts
@@ -1,4 +1,7 @@
 export const NPM_SCOPE =
-    process.env.NODE_ENV === 'production' ? '@genql' : '@shitty-scope-name'
+    process.env.NODE_ENV === 'production'
+        ? 'shitty-scope-name'
+        : '@shitty-scope-name'
 export const NPM_TOKEN = process.env.NPM_TOKEN
 console.assert('NPM_TOKEN', NPM_TOKEN)
+console.log('NODE_ENV', process.env.NODE_ENV)
