fcd64e547cadd63b40bf936d5e0f9a6624d3763d
remorses <beats.by.morse@gmail.com>
2020-05-28 13:33:32 +0200

    genql-runtime/src/client/createClient: better observable

--- genql-runtime/src/client/createClient.ts
+++ genql-runtime/src/client/createClient.ts
@@ -7,7 +7,7 @@ import { chain } from './chain'
 import { LinkedType } from './linkTypeMap'
 import { Fields, requestToGql } from './requestToGql'
 import { MapType } from './typeSelection'
-import { Observable, ObservableLike } from 'zen-observable-ts'
+import { Observable } from 'zen-observable-ts'
 import {
     ClientOptions as SubscriptionOptions,
     SubscriptionClient,
@@ -17,7 +17,7 @@ import { ExecutionResult } from 'graphql'
 export interface Client<QR, QC, Q, MR, MC, M, SR, SC, S> {
     query<R extends QR>(request: R): Promise<MapType<Q, R>>
     mutation<R extends MR>(request: R): Promise<MapType<M, R>>
-    subscription<R extends SR>(request: R): ObservableLike<MapType<S, R>>
+    subscription<R extends SR>(request: R): Observable<MapType<S, R>>
     chain: {
         query: QC
         mutation: MC
@@ -136,7 +136,7 @@ export const createClient = <
 
             const op = requestToGql('subscription', subscriptionRoot, request)
             return Observable.from(subClient.request(op) as any).map(
-                (val: ExecutionResult<any>) => {
+                (val: ExecutionResult<any>): any => {
                     if (val?.errors?.length && val?.errors?.length > 0) {
                         throw new ClientError(
                             `Subscription errors`,
