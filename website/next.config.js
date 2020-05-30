912ccadd4aebc930804631c348a6b4afd0e48e89
remorses <beats.by.morse@gmail.com>
2020-05-30 13:07:48 +0200

    website/next.config: added ts page extension

--- website/next.config.js
+++ website/next.config.js
@@ -2,4 +2,4 @@ const withTM = require('next-transpile-modules')(['genql-cli'])
 const {withDokz} = require('dokz/dist/plugin')
 // Tell webpack to compile the "bar" package
 // https://www.npmjs.com/package/next-transpile-modules
-module.exports = withTM(withDokz({pageExtensions: ['js', 'jsx', 'mdx', 'tsx'],}))
+module.exports = withTM(withDokz({pageExtensions: ['js', 'jsx', 'mdx', 'tsx', 'ts',],}))
