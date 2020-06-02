682c9cb5937bf4c07794ed6f0556f2f37fcc73c6
remorses <beats.by.morse@gmail.com>
2020-06-02 19:43:41 +0200

    some more tests based on github schema

--- integration-tests/generated/createClient.d.ts
+++ integration-tests/generated/createClient.d.ts
@@ -6,13 +6,17 @@ import {
   ClientOptions,
   SubscriptionClientOptions,
 } from 'genql-runtime'
-import { QueryRequest, QueryPromiseChain, Query } from './schema'
+import { QueryRequest, QueryPromiseChain, Query, MutationRequest, MutationPromiseChain, Mutation } from './schema'
 export declare const createClient: (
   options?: ClientOptions,
-) => Client<QueryRequest, QueryPromiseChain, Query, never, never, never>
+) => Client<QueryRequest, QueryPromiseChain, Query, MutationRequest, MutationPromiseChain, Mutation>
 
 export declare const everything: { __scalar: boolean }
 
 export type QueryResult<fields extends QueryRequest> = FieldsSelection<Query, fields>
 
 export declare const generateQueryOp: (fields: QueryRequest) => GraphqlOperation
+
+export type MutationResult<fields extends MutationRequest> = FieldsSelection<Mutation, fields>
+
+export declare const generateMutationOp: (fields: MutationRequest) => GraphqlOperation
--- integration-tests/tests/simple.ts
+++ integration-tests/tests/simple.ts
@@ -1,15 +1,65 @@
-import { generateQueryOp, createClient } from '../generated/createClient'
-import { Choice } from '../generated/schema'
+import {
+    generateQueryOp,
+    createClient,
+    everything,
+} from '../generated/createClient'
 import { prettify } from 'genql-cli/dist/helpers/prettify'
+import { parse } from 'graphql'
 
-describe('simple', () => {
+describe('generate queries', () => {
     it('simple', () => {
         const { query } = generateQueryOp({
-            user: [
-                { username: '', choice: Choice.ONE },
-                { name: 1, company: { ceo: { name: 1 } } },
+            repository: [
+                {
+                    name: 'repo',
+                    owner: 'owner',
+                },
+                {
+                    createdAt: true,
+                    forks: {
+                        edges: {
+                            cursor: true,
+                            node: {
+                                ...everything,
+                            },
+                        },
+                    },
+                },
             ],
         })
+        parse(query)
+        console.log(prettify(query, 'graphql'))
+    })
+    it('many', () => {
+        const { query } = generateQueryOp({
+            repository: [
+                {
+                    name: 'repo',
+                    owner: 'owner',
+                },
+                {
+                    createdAt: true,
+                    forks: {
+                        edges: {
+                            cursor: true,
+                            node: {
+                                ...everything,
+                            },
+                        },
+                    },
+                },
+            ],
+            enterprise: [
+                { slug: 'dsf', invitationToken: '' },
+                {
+                    ...everything,
+                    organizations: {
+                        ...everything,
+                    },
+                },
+            ],
+        })
+        parse(query)
         console.log(prettify(query, 'graphql'))
     })
 })
