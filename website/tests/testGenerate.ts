e77ae57615bbbf4298b040cddc410f25c377d3f8
remorses <beats.by.morse@gmail.com>
2020-06-15 15:49:28 +0200

    website/tests/testGenerate: tests tun

--- website/tests/testGenerate.ts
+++ website/tests/testGenerate.ts
@@ -4,6 +4,7 @@ it('createPackage', async () => {
     await createPackage({
         endpoint: 'https://countries.trevorblades.com',
         name: 'xxxxxxxxx',
+        version: 0,
         callback: async ({ cwd }) => {
             console.log(await runCommand({ cmd: `ls -l`, cwd }))
             console.log(await runCommand({ cmd: `cat package.json`, cwd }))
