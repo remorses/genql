07ea75b21b732af1984b677834d7c6f3194c3227
remorses <beats.by.morse@gmail.com>
2020-06-17 01:22:33 +0200

    better object render

--- genql-cli/src/render/responseTypes/objectType.ts
+++ genql-cli/src/render/responseTypes/objectType.ts
@@ -3,23 +3,48 @@ import { fieldComment, typeComment } from '../common/comment'
 import { RenderContext } from '../common/RenderContext'
 import { renderTyping } from '../common/renderTyping'
 
-export const objectType = (type: GraphQLObjectType | GraphQLInterfaceType, ctx: RenderContext) => {
-  const fieldsMap = type.getFields()
-  const fields = Object.keys(fieldsMap).map(fieldName => fieldsMap[fieldName])
+const INDENTATION = '    '
 
-  if (!ctx.schema) throw new Error('no schema provided')
+export const objectType = (
+    type: GraphQLObjectType | GraphQLInterfaceType,
+    ctx: RenderContext,
+) => {
+    const fieldsMap = type.getFields()
+    const fields = Object.keys(fieldsMap).map(
+        (fieldName) => fieldsMap[fieldName],
+    )
 
-  const typeNames = isObjectType(type) ? [type.name] : ctx.schema.getPossibleTypes(type).map(t => t.name)
+    if (!ctx.schema) throw new Error('no schema provided')
 
-  const fieldStrings = fields
-    .map(f => `${fieldComment(f)}${f.name}${renderTyping(f.type, false, false)}`)
-    .concat([`__typename:${typeNames.length > 0 ? typeNames.map(t => `'${t}'`).join('|') : 'String'}`])
+    const typeNames = isObjectType(type)
+        ? [type.name]
+        : ctx.schema.getPossibleTypes(type).map((t) => t.name)
 
-  const interfaceNames = isObjectType(type) ? type.getInterfaces().map(i => i.name) : []
+    const fieldStrings = fields
+        .map((f) =>
+            `${fieldComment(f)}${f.name}${renderTyping(f.type, false, false)}`
+                .split('\n')
+                .filter(Boolean)
+                .map((l) => INDENTATION + l)
+                .join('\n'),
+        )
+        .concat([
+            `${INDENTATION}__typename: ${
+                typeNames.length > 0
+                    ? typeNames.map((t) => `'${t}'`).join('|')
+                    : 'String'
+            }`,
+        ])
 
-  ctx.addCodeBlock(
-    `${typeComment(type)}export interface ${type.name}${
-      interfaceNames.length > 0 ? ` extends ${interfaceNames.join(',')}` : ''
-    }{${fieldStrings.join(',')}}`,
-  )
+    const interfaceNames = isObjectType(type)
+        ? type.getInterfaces().map((i) => i.name)
+        : []
+
+    ctx.addCodeBlock(
+        `${typeComment(type)}export interface ${type.name}${
+            interfaceNames.length > 0
+                ? ` extends ${interfaceNames.join(',')}`
+                : ''
+        }{\n${fieldStrings.join('\n')}\n}`,
+    )
 }
