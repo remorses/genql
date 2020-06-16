c3ac2a5a6169564485c20941bb6815f0d16c423e
remorses <beats.by.morse@gmail.com>
2020-06-17 01:23:43 +0200

    prettier

--- genql-cli/src/render/requestTypes/objectType.ts
+++ genql-cli/src/render/requestTypes/objectType.ts
@@ -1,49 +1,65 @@
-import { getNamedType, GraphQLInterfaceType, GraphQLObjectType, isEnumType, isInterfaceType, isScalarType } from 'graphql'
+import {
+    getNamedType,
+    GraphQLInterfaceType,
+    GraphQLObjectType,
+    isEnumType,
+    isInterfaceType,
+    isScalarType,
+} from 'graphql'
 import { fieldComment, typeComment } from '../common/comment'
 import { RenderContext } from '../common/RenderContext'
 import { toArgsString } from '../common/toArgsString'
 import { requestTypeName } from './requestTypeName'
 
-export const objectType = (type: GraphQLObjectType | GraphQLInterfaceType, ctx: RenderContext) => {
-  const fields = type.getFields()
-  const fieldStrings = Object.keys(fields).map(fieldName => {
-    const field = fields[fieldName]
+export const objectType = (
+    type: GraphQLObjectType | GraphQLInterfaceType,
+    ctx: RenderContext,
+) => {
+    const fields = type.getFields()
+    const fieldStrings = Object.keys(fields).map((fieldName) => {
+        const field = fields[fieldName]
 
-    const types: string[] = []
-    const resolvedType = getNamedType(field.type)
-    const resolvable = !(isEnumType(resolvedType) || isScalarType(resolvedType))
-    const argsPresent = field.args.length > 0
-    const argsString = toArgsString(field)
-    const argsOptional = !argsString.match(/[^?]:/)
+        const types: string[] = []
+        const resolvedType = getNamedType(field.type)
+        const resolvable = !(
+            isEnumType(resolvedType) || isScalarType(resolvedType)
+        )
+        const argsPresent = field.args.length > 0
+        const argsString = toArgsString(field)
+        const argsOptional = !argsString.match(/[^?]:/)
 
-    if (argsPresent) {
-      if (resolvable) {
-        types.push(`[${argsString},${requestTypeName(resolvedType)}]`)
-      } else {
-        types.push(`[${argsString}]`)
-      }
+        if (argsPresent) {
+            if (resolvable) {
+                types.push(`[${argsString},${requestTypeName(resolvedType)}]`)
+            } else {
+                types.push(`[${argsString}]`)
+            }
+        }
+
+        if (!argsPresent || argsOptional) {
+            if (resolvable) {
+                types.push(`${requestTypeName(resolvedType)}`)
+            } else {
+                types.push('boolean|number')
+            }
+        }
+
+        return `${fieldComment(field)}${field.name}?:${types.join('|')}`
+    })
+
+    if (isInterfaceType(type) && ctx.schema) {
+        ctx.schema
+            .getPossibleTypes(type)
+            .map((t) => `on_${t.name}?:${requestTypeName(t)}`)
+            .forEach((s) => fieldStrings.push(s))
     }
 
-    if (!argsPresent || argsOptional) {
-      if (resolvable) {
-        types.push(`${requestTypeName(resolvedType)}`)
-      } else {
-        types.push('boolean|number')
-      }
-    }
+    fieldStrings.push('__typename?:boolean|number')
+    fieldStrings.push('__scalar?:boolean|number')
 
-    return `${fieldComment(field)}${field.name}?:${types.join('|')}`
-  })
-
-  if (isInterfaceType(type) && ctx.schema) {
-    ctx.schema
-      .getPossibleTypes(type)
-      .map(t => `on_${t.name}?:${requestTypeName(t)}`)
-      .forEach(s => fieldStrings.push(s))
-  }
-
-  fieldStrings.push('__typename?:boolean|number')
-  fieldStrings.push('__scalar?:boolean|number')
-
-  ctx.addCodeBlock(`${typeComment(type)}export interface ${requestTypeName(type)}{${fieldStrings.join(',')}}`)
+    ctx.addCodeBlock(
+        `${typeComment(type)}export interface ${requestTypeName(
+            type,
+        )}{${fieldStrings.join(',')}}`,
+    )
 }
