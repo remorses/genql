e7e24158280fc28355b596222874698f655c78e7
remorses <beats.by.morse@gmail.com>
2020-06-23 13:28:57 +0200

    example-usage/pages/builtin-client/mutation:

--- example-usage/pages/builtin-client/mutation.tsx
+++ example-usage/pages/builtin-client/mutation.tsx
@@ -1,4 +1,4 @@
-import { Stack, Box, Spinner, Input, Button } from '@chakra-ui/core'
+import { Stack, Box, Spinner, Input, Button, useToast } from '@chakra-ui/core'
 import { Hero, SectionTitle, PageContainer } from 'landing-blocks'
 import React, { useState, FormEvent } from 'react'
 import useSWR from 'swr'
@@ -21,7 +21,7 @@ const Page = () => {
             { returning: { ...everything }, ...everything },
         ],
     })
-
+    const toast = useToast()
     return (
         <Stack align='center' spacing='40px' mt='40px'>
             <Hero bullet='Mutation' heading='Mutation Example' subheading='' />
@@ -31,7 +31,23 @@ const Page = () => {
                     value={name}
                     onChange={(e: any) => setName(e.target.value)}
                 />
-                <Button isLoading={loading} onClick={execute}>
+                <Button
+                    isLoading={loading}
+                    onClick={() => {
+                        // TODO validation happens before calling execute, you can wrap execute in your own function
+                        // but there is no clear way on how to handle errors, better pass a function to useMutation
+                        if (name.split(' ').length > 1) {
+                            toast({
+                                status: 'error',
+                                position: 'top',
+                                description: '\n',
+                                title: 'cannot contain spaces',
+                            })
+                            return
+                        }
+                        execute()
+                    }}
+                >
                     Insert User
                 </Button>
                 <Box as='pre'>{data && JSON.stringify(data, null, 4)}</Box>
