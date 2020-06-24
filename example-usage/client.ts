7cde8b11b62774bd14967c7570dedb08792174ad
remorses <beats.by.morse@gmail.com>
2020-06-24 16:26:01 +0200

    example-usage/client: todos

--- example-usage/client.ts
+++ example-usage/client.ts
@@ -38,14 +38,14 @@ export const useMutation = <
     R extends MutationRequest,
     ARG extends any[] = never[] // by default don't let pass any argument
 >(
-    q: R | ((...arg: ARG) => Promise<R>), // TODO the useQuery callback can be a non promise
+    q: R | ((...arg: ARG) => Promise<R>), // TODO react: the useQuery callback can be a non promise
 ): useLazyPromiseOutput<ARG, FieldsSelection<Mutation, R>> => {
     const [execute, res, bo] = useLazyPromise<any>(
         typeof q == 'function' ? q : (q) => q && client.mutation(q),
         {},
     )
     return [typeof q == 'function' ? execute : () => execute(q), res, bo] // TODO change result to data
-    // TODO execute should not throw an error by default, too risky the dev will forget to handle it, just add an option to throw
+    // TODO react: execute should not throw an error by default, too risky the dev will forget to handle it, just add an option to throw
 }
 
 export const useSubscription = <
@@ -62,6 +62,6 @@ export const useSubscription = <
     return useObservable<any>((q) => client.subscription(q), {
         args: [q],
         reducer,
-        // TODO pass the accumulator to useObservable
-    }) // TODO change result to data
+        // TODO react: pass the accumulator to useObservable
+    }) // TODO react: change result to data
 }
