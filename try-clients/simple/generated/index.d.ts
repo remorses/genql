2ffdb75d651a4f65c629441a85556e103304fb05
remorses <beats.by.morse@gmail.com>
2020-07-09 17:04:23 +0200

    try-clients: regen simple

--- try-clients/simple/generated/index.d.ts
+++ try-clients/simple/generated/index.d.ts
@@ -1,7 +1,7 @@
 import { FieldsSelection, GraphqlOperation, ClientOptions, Observable } from 'genql-runtime'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 export * from './schema'
-import { QueryRequest, QueryPromiseChain, Query, MutationRequest, MutationPromiseChain, Mutation } from './schema'
+import { QueryRequest, QueryPromiseChain, Query } from './schema'
 export declare const createClient: (options?: ClientOptions) => Client
 export declare const everything: { __scalar: boolean }
 export declare const version: string
@@ -11,18 +11,11 @@ export interface Client {
 
   query<R extends QueryRequest>(request: R): Promise<FieldsSelection<Query, R>>
 
-  mutation<R extends MutationRequest>(request: R): Promise<FieldsSelection<Mutation, R>>
-
   chain: {
     query: QueryPromiseChain
-
-    mutation: MutationPromiseChain
   }
 }
 
 export type QueryResult<fields extends QueryRequest> = FieldsSelection<Query, fields>
 
 export declare const generateQueryOp: (fields: QueryRequest) => GraphqlOperation
-export type MutationResult<fields extends MutationRequest> = FieldsSelection<Mutation, fields>
-
-export declare const generateMutationOp: (fields: MutationRequest) => GraphqlOperation
