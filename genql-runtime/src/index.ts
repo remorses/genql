56c9746bb83f6bd9240514209ace65182ee20c03
bump-version <bump@version.com>
2020-07-16 20:46:40 +0000

    Genql-runtime Version 1.1.22 [skip ci]

--- genql-runtime/src/index.ts
+++ genql-runtime/src/index.ts
@@ -13,7 +13,7 @@ export const everything = {
 }
 
 // [bump if genql-runtime]
-export const version = '1.1.21'
+export const version = '1.1.22'
 
 export function assertSameVersion(generatedWithVersion) {
     if (generatedWithVersion && generatedWithVersion.trim() != version.trim()) {
