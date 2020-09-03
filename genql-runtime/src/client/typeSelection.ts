92ad69729e65f27c13644abb5e0fa5fa0d87d664
remorses <beats.by.morse@gmail.com>
2020-09-03 19:16:03 +0200

    genql-runtime/src/client/typeSelection: added more comments

--- genql-runtime/src/client/typeSelection.ts
+++ genql-runtime/src/client/typeSelection.ts
@@ -1,76 +1,58 @@
-type Scalar = string | number | Date | boolean | null | undefined
-
-// SRC is the response type, DEST is the request type
-// export type FieldsSelection<SRC extends Anify<DST>, DST> = DST extends never
-//     ? never
-//     : DST extends Scalar
-//     ? SRC // TODO query fields selection is done only on the top level fields, if i want to use recursively i must handle the on_ cases
-//     : Anify<Omit<DST, '__typename' | '__scalar'>> extends Anify<
-//           NonNullable<SRC>
-//       >
-//     ? Pick<SRC, keyof DST>
-//     : SRC
-
-type Anify<T> = { [P in keyof T]?: any }
-
 //////////////////////////////////////////////////
 
-// type requestType =
-//     | Record<string, any>
-//     | [Record<string, any>, Record<string, any>]
-
-// type ObjectSelection<SRC extends Anify<DST>, DST> = Anify<
-//     Omit<DST, '__typename' | '__scalar'>
-// > extends Anify<NonNullable<SRC>>
-//     ? Pick<SRC, keyof DST>
-//     : SRC
+export type FieldsSelection<SRC extends Anify<DST>, DST> = Pick<
+    {
+        // using keyof SRC to maintain ?: relations of SRC type
+        [Key in keyof SRC]: Key extends keyof DST
+            ? DST[Key] extends [any, infer PAYLOAD]
+                ? HandleScalars<SRC[Key], PAYLOAD> // continue processing only on the selection field
+                : HandleScalars<SRC[Key], DST[Key]>
+            : never
+    },
+    keyof DST
+>
 
 type HandleScalars<SRC, DST> = SRC extends undefined
     ? never
-    : DST extends true | 1
+    : DST extends true | 1 // directly return scalar types
     ? SRC
-    : SRC extends Scalar
+    : SRC extends Scalar // directly return scalar types
     ? SRC
-    : DST extends { __scalar?: any }
+    : SRC extends { __isUnion?: any } // process types with __isUnion field
+    ? Handle__isUnion<SRC, DST>
+    : DST extends { __scalar?: any } // process types with __scalar field // TODO how to handle unions and __scalar together?
     ? Handle__scalar<SRC, DST>
-    : SRC extends { __isUnion?: any }
-    ? HandleUnions<SRC, DST>
-    : FieldsSelection<SRC, DST>
+    : FieldsSelection<SRC, DST> // process normal object types
 
 type Handle__scalar<SRC extends Anify<DST>, DST> = SRC extends undefined
     ? never
     : Omit<
           Pick<
+              // continue processing fields that are in DST, directly pass SRC type if not in DST
               {
                   [Key in keyof SRC]: Key extends keyof DST
                       ? HandleScalars<SRC[Key], DST[Key]>
                       : SRC[Key]
               },
+              // remove fields that are not scalars or are not in DST
               {
-                  [Key in keyof SRC]: NonNullable<SRC[Key]> extends Scalar
+                  [Key in keyof SRC]: SRC[Key] extends Scalar
                       ? Key
                       : Key extends keyof DST
                       ? Key
                       : never
               }[keyof SRC]
           >,
-          '__scalar'
+          FieldsToRemove
       >
 
-// unions are very dumb
-// TODO add the __isUnion key to every union type
-type HandleUnions<SRC extends Anify<DST>, DST> = SRC extends undefined
+// TODO response union types are very dumb
+type Handle__isUnion<SRC extends Anify<DST>, DST> = SRC extends undefined
     ? never
-    : Omit<SRC, '__isUnion'> // TODO apply a FieldsSelection inside every on_ and on the top level fields
+    : Omit<SRC, FieldsToRemove> // just return the union type
 
-// TODO handle on_ keys
-export type FieldsSelection<SRC extends Anify<DST>, DST> = Pick<
-    {
-        [Key in keyof SRC]: Key extends keyof DST
-            ? DST[Key] extends [any, infer PAYLOAD]
-                ? HandleScalars<SRC[Key], PAYLOAD>
-                : HandleScalars<SRC[Key], DST[Key]>
-            : never
-    },
-    keyof DST
->
+type Scalar = string | number | Date | boolean | null | undefined
+
+type Anify<T> = { [P in keyof T]?: any }
+
+type FieldsToRemove = '__isUnion' | '__scalar'
