46afd1bc94bf03e112fa8e1771160d750abb8e7f
remorses <beats.by.morse@gmail.com>
2020-10-06 20:14:04 +0200

    website/components/RunKit: fix runkit

--- website/components/RunKit.tsx
+++ website/components/RunKit.tsx
@@ -3,7 +3,7 @@ import Embed from 'react-runkit'
 
 export const RunKit = ({ children, title = '', ...rest }) => {
     return (
-        <Stack borderRadius='md' borderWidth='1px' shadow='lg' mx='20px' my='40px' {...rest}>
+        <Stack align='stretch' width='100%' borderRadius='md' borderWidth='1px' shadow='lg' mx='20px' my='40px' {...rest}>
             <Embed source={String(children).trim()} title={title} />
         </Stack>
     )
