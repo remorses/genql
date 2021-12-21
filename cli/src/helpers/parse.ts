4d765739f8476cdf3c7bdb2c30d7605d294666a9
Jonas Strassel <info@jonas-strassel.de>
2021-12-21 14:26:14 +0000

    chore: cleanup

--- cli/src/helpers/parse.ts
+++ cli/src/helpers/parse.ts
@@ -1,15 +1,13 @@
-export function parseColonSeparatedStrings(array: string[]) {
+export function parseColonSeparatedStrings(array: string[] = []) {
     let obj = {}
-    if (array) {
-        for (let h of array) {
-            const key = String(h).substring(0, h.indexOf(":")).trim();
-            const value = h.substring(h.indexOf(":") + 1).trim();
-            if (!key || !value) {
-                console.error(`cannot parse string '${h}' as key:value pair`)
-                process.exit(1)
-            }
-            obj[key] = value
+    for (let h of array) {
+        const key = String(h).substring(0, h.indexOf(":")).trim();
+        const value = h.substring(h.indexOf(":") + 1).trim();
+        if (!key || !value) {
+            console.error(`cannot parse string '${h}' as key:value pair`)
+            process.exit(1)
         }
+        obj[key] = value
     }
     return obj
 }
