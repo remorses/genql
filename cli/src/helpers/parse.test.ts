55e3a41db1c7a6aceb147a137b201555d361baec
Jonas Strassel <info@jonas-strassel.de>
2021-12-21 14:16:51 +0000

    fix: allow multiple colons ins headers

---
+++ cli/src/helpers/parse.test.ts
@@ -0,0 +1,32 @@
+import { parseColonSeparatedStrings } from "./parse";
+
+describe("header parsing", () => {
+    test("Bearer Token", () => {
+    const header = parseColonSeparatedStrings(["Authorization: Bearer 1234"]);
+    expect(header).toEqual({
+        Authorization: "Bearer 1234",
+    });
+})
+
+test("Referer", () => {
+    const header = parseColonSeparatedStrings(["Referer: https://www.xyz.com"]);
+    expect(header).toEqual({
+        Referer: "https://www.xyz.com",
+    });
+})
+
+test("Multiple", () => {
+    const header = parseColonSeparatedStrings(["Referer: https://www.xyz.com", "Accept: application/json"]);
+    expect(header).toEqual({"Accept": "application/json", "Referer": "https://www.xyz.com"});
+})
+});
+
+describe("scalar parsing", () => {
+    test("DateTime:string", ()=> {
+        const scalar = parseColonSeparatedStrings(["DateTime:string"]);
+    
+        expect(scalar).toEqual({
+            DateTime: "string",
+        })
+    })
+})
---
+++ cli/src/helpers/parse.ts
@@ -0,0 +1,15 @@
+export function parseColonSeparatedStrings(array: string[]) {
+    let obj = {}
+    if (array) {
+        for (let h of array) {
+            const key = String(h).substring(0, h.indexOf(":")).trim();
+            const value = h.substring(h.indexOf(":") + 1).trim();
+            if (!key || !value) {
+                console.error(`cannot parse string '${h}' as key:value pair`)
+                process.exit(1)
+            }
+            obj[key] = value
+        }
+    }
+    return obj
+}
