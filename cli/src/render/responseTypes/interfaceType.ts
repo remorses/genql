3a790dde4711663c863c9d839e8d9d06f30e83cc
remorses <beats.by.morse@gmail.com>
2022-04-10 13:29:03 +0200

    fixed #81

--- cli/src/render/responseTypes/interfaceType.ts
+++ cli/src/render/responseTypes/interfaceType.ts
@@ -1,6 +1,7 @@
 import { GraphQLUnionType, GraphQLInterfaceType } from 'graphql'
 import { RenderContext } from '../common/RenderContext'
 import { typeComment } from '../common/comment'
+import { objectType } from './objectType'
 
 export const interfaceType = (
     type: GraphQLInterfaceType,
@@ -10,11 +11,15 @@ export const interfaceType = (
         throw new Error('schema is required to render unionType')
     }
     const typeNames = ctx.schema.getPossibleTypes(type).map((t) => t.name)
-    ctx.addCodeBlock(
-        `${typeComment(type)}export type ${type.name} = (${typeNames.join(
-            ' | ',
-        )}) & { __isUnion?: true }`,
-    )
+    if (!typeNames.length) {
+        objectType(type, ctx)
+    } else {
+        ctx.addCodeBlock(
+            `${typeComment(type)}export type ${type.name} = (${typeNames.join(
+                ' | ',
+            )}) & { __isUnion?: true }`,
+        )
+    }
 }
 
 // interface should produce an object like
--- cli/src/render/typeGuards/renderTypeGuards.ts
+++ cli/src/render/typeGuards/renderTypeGuards.ts
@@ -10,7 +10,7 @@ import { RenderContext } from '../common/RenderContext'
 const renderTypeGuard = (target: string, possible: string[], mode) =>
     mode == 'ts'
         ? `
-const ${target}_possibleTypes = [${possible.map((t) => `'${t}'`).join(',')}]
+const ${target}_possibleTypes: string[] = [${possible.map((t) => `'${t}'`).join(',')}]
 export const is${target} = (obj?: { __typename?: any } | null): obj is ${target} => {
   if (!obj?.__typename) throw new Error('__typename is missing in "is${target}"')
   return ${target}_possibleTypes.includes(obj.__typename)
