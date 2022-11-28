64cd5dd079cb5d74301953976d8c91e700919bde
remorses <beats.by.morse@gmail.com>
2022-11-28 21:53:39 +0100

    website/src: stuff

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -14,7 +14,7 @@ import {
     BG,
 } from '../constants'
 import 'baby-i-am-faded/styles.css'
-import './global.css'
+import '../global.css'
 import { Bg } from '@app/components/bg'
 
 // 'radial-gradient( 37.86% 77.79% at 50% 100%, rgba(113,128,150,0.25) 0%, rgba(113,128,150,0) 100% ), linear-gradient(180deg,#1a202c 0%,#2d3748 100%), linear-gradient(180deg,#0d0f14 0%,rgba(27,32,43,0) 100%),#2f3747'
