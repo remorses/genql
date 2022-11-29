8631ed34e2827df043ef8eb9a9aca90f5a038c90
remorses <beats.by.morse@gmail.com>
2022-11-29 15:51:27 +0100

    try-clients/tests: nn

---
+++ try-clients/tests/real-package.ts
@@ -0,0 +1,20 @@
+import { parse } from 'graphql'
+import assert from 'assert'
+import { createClient, everything } from '@shitty-scope-name/countries'
+import fetch from 'isomorphic-unfetch'
+
+describe('countries real package', () => {
+    const client = createClient({})
+    it('simple query', async () => {
+        const res = await client.query({
+            countries: [
+                { filter: { code: { eq: 'US' } } },
+                {
+                    ...everything,
+                },
+            ],
+        })
+        console.log(res)
+        assert(res)
+    })
+})
