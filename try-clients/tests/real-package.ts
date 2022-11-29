55bf4e001da97ca75443dd338080a72ea77a4268
remorses <beats.by.morse@gmail.com>
2022-11-29 15:08:31 +0100

    try-clients/tests/real-package: works

--- try-clients/tests/real-package.ts
+++ try-clients/tests/real-package.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@shitty-scope-name/countries'
 
-test('countries client', async () => {
-    const client = createClient({})
-    const res = await client.query({
-        countries: [{ filter: { code: { regex: '.*' } } }, { code: true }],
+describe('countries', () => {
+    it('countries client', async () => {
+        const client = createClient({})
+        const res = await client.query({
+            countries: [{ filter: { code: { regex: '.*' } } }, { code: true }],
+        })
+        console.log(res)
     })
-    console.log(res)
 })
